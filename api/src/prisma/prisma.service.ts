408b38b809103b186d00d6bfd56106651b076dfd
Rodrigo Posser <rodrigopossercarvalho@gmail.com>
2026-05-23 18:17:14 -0300

    Simplify Prisma: remove adapter-pg, use standard url=env() in schema

--- api/src/prisma/prisma.service.ts
+++ api/src/prisma/prisma.service.ts
@@ -1,21 +1,11 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor(config: ConfigService) {
-    const connectionString = config.get<string>('DATABASE_URL')!;
-    const pool = new Pool({ connectionString });
-    const adapter = new PrismaPg(pool);
-    super({ adapter });
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
